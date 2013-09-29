4e01659c54e342197d9709558e226d89dcf79933
Mo Wang <mo.hy.wang@gmail.com>
2013-09-28 17:53:41 -0700

    fix the deface

--- WebRtc/app.js
+++ WebRtc/app.js
@@ -7,6 +7,14 @@
     //var connConfig = {host: '192.168.0.1', port: 9000};
     var connConfig = { key: '5bkpny04pqw4gqfr' };
 
+    var payload = {
+        startX: 0,
+        startY: 0,
+        currentX: 0,
+        currentY: 0,
+        local: true,
+};
+
     function AppViewModel() {
 
         var self = this;
@@ -26,8 +34,8 @@
         self.otherFinalCanvas = $('#otherFinalCanvas')[0];
         self.connection = null;
 
-        new RtcCanvas('#myRawCanvas');
-        new RtcCanvas('#otherRawCanvas');
+        new RtcCanvas('#myRawCanvas', false);
+        new RtcCanvas('#otherRawCanvas', true);
 
         self.connectToPeer = function () {
             console.log(self.connectpeerid());
@@ -123,21 +131,50 @@
         if (viewModel.otherVideo) {
             otherRawCtx.drawImage(viewModel.otherVideo, 0, 0, width, height);
         }
-        
-        var inData = myRawCtx.getImageData(0, 0, width, height);
-        var outData = createImageData(myRawCtx, width, height);
 
-        solarize(inData.data, outData.data, width, height);
-        myRawCtx.putImageData(outData, 0, 0);
+        var local = payload.local;
 
-        inData = otherRawCtx.getImageData(0, 0, width, height);
-        outData = createImageData(otherRawCtx, width, height);
-        
-        solarize(inData.data, outData.data, width, height);
-        otherRawCtx.putImageData(outData, 0, 0);
+        // remote
+        if (!local) {
+            doEffect(myRawCtx);
+        } else {
+            doEffect(otherRawCtx);
+        }
 
         //onFrame(canvas);
         requestAnimationFrame(refresh);
+    }
+    
+    function doEffect(ctx) {
+        var inData = ctx.getImageData(0, 0, width, height);
+        var outData = createImageData(ctx, width, height);
+
+        var aa = new area(0, 0, 0, 0);
+        
+        var startx = payload.startX;
+        var starty = payload.startY;
+        var currx = payload.currentX;
+        var curry = payload.currentY;
+
+        if (startx < currx) {
+            aa.x = startx;
+            aa.width = currx - startx;
+        } else {
+            aa.x = currx;
+            aa.width = startx - currx;
+        }
+
+        if (starty < curry) {
+            aa.y = starty;
+            aa.height = curry - starty;
+        } else {
+            aa.y = curry;
+            aa.height = starty - curry;
+        }
+
+        applyArea(inData.data, outData.data, width, height, aa, applyEdge2);
+
+        ctx.putImageData(outData, 0, 0);
     }
     
     requestAnimationFrame(refresh);
@@ -159,35 +196,12 @@
     function setupCommandBinding(commandConnection) {
         commandConnection.on('open', function () {
             commandConnection.on('data', function (data) {
-                console.log(data);
+                //console.log(data);
+                payload = data;
                 viewModel.receiveCommand(data);
             });
         });
     }
-
-    var solarize = function (inData, outData, width, height, options, progress) {
-        var n = width * height * 4,
-            prog, lastProg = 0,
-            r, g, b;
-
-        for (i = 0; i < n; i += 4) {
-            r = inData[i];
-            g = inData[i + 1];
-            b = inData[i + 2];
-
-            outData[i] = r > 127 ? 255 - r : r;
-            outData[i + 1] = g > 127 ? 255 - g : g;
-            outData[i + 2] = b > 127 ? 255 - b : b;
-            outData[i + 3] = inData[i + 3];
-
-            if (progress) {
-                prog = (i / n * 100 >> 0) / 100;
-                if (prog > lastProg) {
-                    lastProg = progress(prog);
-                }
-            }
-        }
-    };
 
     function createImageData(ctx, width, height) {
         if (ctx.createImageData) {
@@ -197,15 +211,14 @@
         }
     }
 
-    function RtcCanvas(selector) {
+    function RtcCanvas(selector, canDeface) {
         var self = this;
         self.$element = $(selector);
-        var isMouseDown = false;
-        var ctx = self.$element[0].getContext('2d');
-
         setElementSize(self.$element[0], width, height);
 
-        var startX, startY;
+        if (!canDeface) return;
+
+        var startX, startY, isMouseDown = false;
 
         self.$element
             .mousedown(function (evt) {
@@ -213,22 +226,43 @@
                 isMouseDown = true;
                 startX = evt.offsetX;
                 startY = evt.offsetY;
+
+               payload = {
+                    startX: startX,
+                    startY: startY,
+                    currentX: startX,
+                    currentY: startY,
+                    local: true
+               };
+                
+               sendData({
+                   startX: startX,
+                   startY: startY,
+                   currentX: startX,
+                   currentY: startY,
+                   local: false
+               });
             })
             .mouseup(function () {
                 isMouseDown = false;
             })
             .mousemove(function (evt) {
                 if (!isMouseDown) return;
-                
-                //console.log("x: " + x + " y: " + y);
-                //var pixelData = ctx.getImageData(x, y, 1, 1).data;
-                //console.log(pixelData);
+               
+                payload = {
+                    startX: startX,
+                    startY: startY,
+                    currentX: evt.offsetX,
+                    currentY: evt.offsetY,
+                    local: true
+                };
 
                 sendData({
                     startX: startX,
                     startY: startY,
                     currentX: evt.offsetX,
-                    currentY: evt.offsetY
+                    currentY: evt.offsetY,
+                    local: false
                 });
             });
 
@@ -253,6 +287,179 @@
     
     function sendData(data) {
         viewModel.connection.send(data);
+    }
+    
+    function area(x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        this.width = w;
+        this.height = h;
+
+        this.contains = function (xcoord, ycoord) {
+            return xcoord >= this.x && ycoord >= this.y && xcoord <= (this.x + this.width) && ycoord <= (this.y + this.height);
+        }
+
+        this.empty = function () {
+            return this.width == 0 && this.height == 0;
+        }
+    }
+    
+    function applyArea(inData, outData, width, height, area, filter) {
+        var xstart = area.x;
+        var ystart = area.y;
+        var xend = xstart + area.width;
+        var yend = ystart + area.height;
+
+
+        for (x = 0; x < width; x++) {
+            for (y = 0; y < height; y++) {
+                var offset = (y * width * 4) + (x * 4);
+                var r = inData[offset];
+                var g = inData[offset + 1];
+                var b = inData[offset + 2];
+                if (area.contains(x, y) && !area.empty()) {
+                    filter(inData, outData, width, height, x, y);
+                } else {
+                    outData[offset] = r;
+                    outData[offset + 1] = g;
+                    outData[offset + 2] = b;
+                    outData[offset + 3] = inData[offset + 3];
+                }
+            }
+        }
+    }
+
+    function applyGray(inData, outData, width, height, x, y) {
+        var offset = (y * width * 4) + (x * 4);
+        var r = inData[offset];
+        var g = inData[offset + 1];
+        var b = inData[offset + 2];
+
+        var gray = (r + g + b) / 3;
+        outData[offset] = gray;
+        outData[offset + 1] = gray;
+        outData[offset + 2] = gray;
+        outData[offset + 3] = inData[offset + 3];
+    }
+
+    function applyBw(inData, outData, width, height, x, y) {
+        var ny = y + 1;
+        var nx = x + 1;
+
+        var offset = (y * width * 4) + (x * 4);
+        var r = inData[offset];
+        var g = inData[offset + 1];
+        var b = inData[offset + 2];
+
+        var gray = (r + g + b) / 3;
+        if (gray < 128) {
+            gray = 0;
+        } else {
+            gray = 255;
+        }
+        outData[offset] = gray;
+        outData[offset + 1] = gray;
+        outData[offset + 2] = gray;
+        outData[offset + 3] = inData[offset + 3];
+    }
+
+    function applyNAvg(inData, outData, width, height, x, y) {
+        var r = 0;
+        var g = 0;
+        var b = 0;
+        var size = 0;
+        var loc = ((y) * width * 4) + ((x) * 4);
+
+        for (i = -3; i <= 3; i++) {
+            for (j = -3; j <= 3; j++) {
+                if ((x + i) < 0 || (y + j) < 0) {
+                    continue;
+                }
+                var offset = ((y + j) * width * 4) + ((x + i) * 4);
+                r += inData[offset];
+                g += inData[offset + 1];
+                b += inData[offset + 2];
+                size++;
+            }
+        }
+
+        outData[loc] = (r / size);
+        outData[loc + 1] = (g / size);
+        outData[loc + 2] = (b / size);
+        outData[loc + 3] = inData[loc + 3];
+    }
+
+    function applyEdge(inData, outData, width, height, x, y) {
+        var gx = [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]];
+        var gy = [[1, 2, 1], [0, 0, 0], [-1, -2, -1]];
+
+        var nx = 0, ny = 0;
+        var size = 0;
+        var loc = ((y) * width * 4) + ((x) * 4);
+
+        for (i = -1; i <= 1; i++) {
+            for (j = -1; j <= 1; j++) {
+                if ((x + i) < 0 || (y + j) < 0) {
+                    continue;
+                }
+
+                var offset = ((y + j) * width * 4) + ((x + i) * 4);
+                var gray = (inData[offset] + inData[offset + 1] + inData[offset + 2]) / 3;
+                nx += gx[j + 1][i + 1] * gray;
+                ny += gy[j + 1][i + 1] * gray;
+            }
+        }
+
+        var pix = 255;
+        if (((nx * nx) + (ny * ny)) > (128 * 128)) {
+            pix = 0;
+        }
+
+        pix = 255 - ((((nx * nx) + (ny * ny)) / (128 * 128)) * 255);
+        if (pix <= 128) {
+            outData[loc] = pix;
+            outData[loc + 1] = pix;
+            outData[loc + 2] = pix;
+        } else {
+            outData[loc] = inData[loc];
+            outData[loc + 1] = inData[loc + 1];
+            outData[loc + 2] = inData[loc + 2];
+        }
+        outData[loc + 3] = inData[loc + 3];
+    }
+
+    function applyEdge2(inData, outData, width, height, x, y) {
+        var gx = [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]];
+        var gy = [[1, 2, 1], [0, 0, 0], [-1, -2, -1]];
+
+        var nx = 0, ny = 0;
+        var size = 0;
+        var loc = ((y) * width * 4) + ((x) * 4);
+
+        for (i = -1; i <= 1; i++) {
+            for (j = -1; j <= 1; j++) {
+                if ((x + i) < 0 || (y + j) < 0) {
+                    continue;
+                }
+
+                var offset = ((y + j) * width * 4) + ((x + i) * 4);
+                var gray = (inData[offset] + inData[offset + 1] + inData[offset + 2]) / 3;
+                nx += gx[j + 1][i + 1] * gray;
+                ny += gy[j + 1][i + 1] * gray;
+            }
+        }
+
+        var pix = 255;
+        if (((nx * nx) + (ny * ny)) > (128 * 128)) {
+            pix = 0;
+        }
+
+        pix = 255 - ((((nx * nx) + (ny * ny)) / (128 * 128)) * 255);
+
+        outData[loc] = pix;
+        outData[loc + 1] = pix;
+        outData[loc + 2] = pix;
+        outData[loc + 3] = inData[loc + 3];
     }
 
 })();
